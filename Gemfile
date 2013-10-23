b95d5042afb7297e8e50f68bbe131afbe571662d
jongrover <hello@jonathangrover.com>
2013-10-23 16:55:38 -0400

    pull out heroku needs

--- Gemfile
+++ Gemfile
@@ -1,19 +1,9 @@
 source 'https://rubygems.org'
 
-ruby '2.0.0'
-
 gem 'data_mapper'
 gem 'sinatra'
+gem 'sqlite3'
+gem 'dm-sqlite-adapter'
+gem 'sinatra-reloader'
+gem 'pry'
 
-group :development, :test  do
-  gem 'sqlite3'
-  gem 'dm-sqlite-adapter'
-  gem 'sinatra-reloader'
-  gem 'pry'
-end
-
-group :production  do
-  gem 'pg'
-  gem 'dm-postgres-adapter'
-  gem 'therubyracer'
-end
--- config/environment.rb
+++ config/environment.rb
@@ -1,13 +1,9 @@
 require 'bundler'
 
-Bundler.require(:default, ENV['RACK_ENV'].to_sym)
+Bundler.require
 
-if ENV['RACK_ENV'] == 'production'
-  DataMapper.setup(:default, ENV['DATABASE_URL'])
-else # development or testing only
-  DataMapper::Logger.new($stdout, :debug)
-  DataMapper.setup(:default, 'sqlite:///Users/jonathangrover/dev/fis/sinatra-pigeon/db/pigeons.db')  
-end
+DataMapper::Logger.new($stdout, :debug)
+DataMapper.setup(:default, 'sqlite:///Users/jonathangrover/dev/fis/sinatra-pigeon/db/pigeons.db')  
 
 require_relative '../app/models/pigeon'
 require_relative '../app/controllers/pigeons_controller'
