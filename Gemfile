1d284058b9f3c9d4720f375d871a2be7bc72488c
jongrover <hello@jonathangrover.com>
2013-10-23 15:30:16 -0400

    make heroku postgres happy to use datamapper

--- Gemfile
+++ Gemfile
@@ -1,5 +1,7 @@
 source 'https://rubygems.org'
 
+ruby '2.0.0'
+
 gem 'data_mapper'
 gem 'sinatra'
 
--- config/environment.rb
+++ config/environment.rb
@@ -1,8 +1,12 @@
 require 'bundler'
-Bundler.require
 
-DataMapper::Logger.new($stdout, :debug)
-DataMapper.setup(:default, 'sqlite:///Users/jonathangrover/dev/fis/sinatra-pigeon/db/pigeons.db')
+Bundler.require(:default, ENV['RACK_ENV'].to_sym)
+if ENV['RACK_ENV'] == 'production'
+  DataMapper.setup(:default, ENV['DATABASE_URL'])
+else # development or testing only
+  DataMapper::Logger.new($stdout, :debug)
+  DataMapper.setup(:default, 'sqlite:///Users/jonathangrover/dev/fis/sinatra-pigeon/db/pigeons.db')  
+end
 
 require_relative '../app/models/pigeon'
 require_relative '../app/controllers/pigeons_controller'
