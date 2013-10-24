1e6fae456390bc39241eb9fbd218e9715157d9c5
jongrover <hello@jonathangrover.com>
2013-10-23 20:13:46 -0400

    make db path dynamic

--- config/environment.rb
+++ config/environment.rb
@@ -3,7 +3,7 @@ require 'bundler'
 Bundler.require
 
 DataMapper::Logger.new($stdout, :debug)
-DataMapper.setup(:default, 'sqlite:///Users/jonathangrover/dev/fis/sinatra-pigeon/db/pigeons.db')  
+DataMapper.setup(:default, "sqlite://#{Dir.pwd}/db/pigeons.db")
 
 require_relative '../app/models/pigeon'
 require_relative '../app/controllers/pigeons_controller'
