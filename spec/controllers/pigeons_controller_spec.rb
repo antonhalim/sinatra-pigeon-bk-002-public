176e8ee6a363246df87e36873eb4a6198fce7165
Katie Hoffman <ktahoffman@gmail.com>
2014-07-01 11:05:31 -0400

    reconfigures config, addds spec folder with some controller tests

---
+++ spec/controllers/pigeons_controller_spec.rb
@@ -0,0 +1,38 @@
+require 'spec_helper'
+
+describe "PigeonsController" do
+  describe "GET /" do
+    before do
+      Pigeon.create(name: "Cher Ami" , color: "red, grey", lives: "Paris"        , gender: "female")
+      Pigeon.create(name: "G. I. Joe", color: "pink, tan", lives: "Great Britain", gender: "male"  )
+      get '/'
+    end
+
+    it "responds with a 200 status code" do
+      expect(last_response).to be_ok
+    end
+
+    it "renders the pigeon index template" do
+      expect(last_response.body).to include("Cher Ami")
+      expect(last_response.body).to include("G. I. Joe")
+    end
+  end
+
+  describe "GET /pigeons/:id/" do
+    before do
+      cher = Pigeon.create(name:  "Cher Ami", color: "red, grey", lives: "Paris",         gender: "female")
+      get "/actors/#{cher.id}"
+    end
+
+    it "responds with a 200 status code" do
+      expect(last_response).to be_ok
+    end
+
+    it "renders the show page for the pigeons's list of attributes" do
+      expect(last_response.body).to include("Cher Ami")
+      expect(last_response.body).to include("red, grey")
+      expect(last_response.body).to include("Paris")
+    end
+  end
+
+end
